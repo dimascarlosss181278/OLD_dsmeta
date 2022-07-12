80a81dddf5684db6c8617744746d2d8895e40251
Dimas Carlos <dimascarlosss@hotmail.com>
2022-07-12 00:03:44 -0300

    Firts component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
    return(
-    <div style={{
-     backgroundImage : `url("https://www.instagram.com/p/CABqwTmjJEq/?utm_source=ig_web_copy_link")`
-    }}>
+    <div>
     <h1>Teste APP Dimas</h1>
     <h1>Semana Spring React</h1>
     <h1>Backend (Java/Spring Boot) e Frontend (ReactJS, CSS, HTML, Typescript)</h1>
+    <NotificationButton/>
     </div>
     )
 }
